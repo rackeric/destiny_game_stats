763e131913e1d133cf9632e981b216e5c7fa6e5b
rackeric <eric.hernandez@rackspace.com>
2015-10-13 12:05:04 -0500

    now returns error page with message when bungie API is down

--- destiny_stats.py
+++ destiny_stats.py
@@ -13,6 +13,9 @@ def index():
     advisors_url = 'http://www.bungie.net/Platform/Destiny/Advisors/?definitions=true'
     advisors = requests.get(advisors_url, headers=headers, cookies=cookies)
 
+    if advisors.json()['ErrorCode'] is not 1:
+        return render_template('error.html', member=advisors.json())
+
     return render_template('index.html', advisors=advisors.json())
 
 @app.route('/summary')
@@ -28,29 +31,33 @@ def summary():
     search_url = 'http://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/all/' + myUsername + '/'
     member = requests.get(search_url, headers=headers, cookies=cookies)
 
+    if member.json()['ErrorCode'] is not 1:
+        return render_template('error.html', member=member.json())
+
     member_id = member.json().get('Response')[0]['membershipId']
 
     acct_summary_url = 'http://www.bungie.net/Platform/Destiny/1/Account/' + member_id + '/Summary/?definitions=true'
     account = requests.get(acct_summary_url, headers=headers, cookies=cookies)
 
-    #  THIS TAKES FOREVER!!!
-    #  FIND A BETTER WAY!!!!
-    BUCKET_VAULT_WEAPONS = ''
-    BUCKET_VAULT_ARMOR = ''
-    BUCKET_VAULT_ITEMS = ''
-    for bucket in account.json()['Response']['definitions']['buckets']:
-        if account.json()['Response']['definitions']['buckets'][bucket]['bucketIdentifier'] == "BUCKET_VAULT_WEAPONS":
-            BUCKET_VAULT_WEAPONS = bucket
-            print "hit 1"
-            print BUCKET_VAULT_WEAPONS
-        elif account.json()['Response']['definitions']['buckets'][bucket]['bucketIdentifier'] == "BUCKET_VAULT_ARMOR":
-            BUCKET_VAULT_ARMOR = bucket
-            print "hit 2"
-            print BUCKET_VAULT_ARMOR
-        elif account.json()['Response']['definitions']['buckets'][bucket]['bucketIdentifier'] == "BUCKET_VAULT_ITEMS":
-            BUCKET_VAULT_ITEMS = bucket
-            print "hit 3"
-            print BUCKET_VAULT_ITEMS
+    if account.json()['ErrorCode'] is 1:
+        #  THIS TAKES FOREVER!!!
+        #  FIND A BETTER WAY!!!!
+        BUCKET_VAULT_WEAPONS = ''
+        BUCKET_VAULT_ARMOR = ''
+        BUCKET_VAULT_ITEMS = ''
+        for bucket in account.json()['Response']['definitions']['buckets']:
+            if account.json()['Response']['definitions']['buckets'][bucket]['bucketIdentifier'] == "BUCKET_VAULT_WEAPONS":
+                BUCKET_VAULT_WEAPONS = bucket
+                print "hit 1"
+                print BUCKET_VAULT_WEAPONS
+            elif account.json()['Response']['definitions']['buckets'][bucket]['bucketIdentifier'] == "BUCKET_VAULT_ARMOR":
+                BUCKET_VAULT_ARMOR = bucket
+                print "hit 2"
+                print BUCKET_VAULT_ARMOR
+            elif account.json()['Response']['definitions']['buckets'][bucket]['bucketIdentifier'] == "BUCKET_VAULT_ITEMS":
+                BUCKET_VAULT_ITEMS = bucket
+                print "hit 3"
+                print BUCKET_VAULT_ITEMS
 
 
     return render_template('summary.html',
